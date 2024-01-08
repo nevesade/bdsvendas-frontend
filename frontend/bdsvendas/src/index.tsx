5424fc86162b194772b8c92d27216408176b2acd
nevesade <adenasiocarvalho19@gmail.com>
2024-01-08 20:04:11 +0000

    Fixed eslint

--- frontend/bdsvendas/src/index.tsx
+++ frontend/bdsvendas/src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -5,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
